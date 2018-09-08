355bafbc9435e1c108e6aa386007dc18970da957
Christian Lück <christian@lueck.tv>
2018-09-08 16:52:33 +0200

    Improve documentation

--- src/Client.php
+++ src/Client.php
@@ -7,7 +7,17 @@ use Clue\React\Soap\Protocol\ClientDecoder;
 use Clue\React\Soap\Protocol\ClientEncoder;
 use Psr\Http\Message\ResponseInterface;
 use React\Promise\Deferred;
+use React\Promise\PromiseInterface;
 
+/**
+ * The `Client` class is responsible for communication with the remote SOAP
+ * WebService server.
+ *
+ * If you want to call RPC functions, see below for the [`Proxy`](#proxy) class.
+ *
+ * Note: It's recommended (and easier) to wrap the `Client` in a [`Proxy`](#proxy) instance.
+ * All public methods of the `Client` are considered *advanced usage*.
+ */
 final class Client
 {
     private $wsdl;
@@ -40,6 +50,25 @@ final class Client
         $this->decoder = $decoder;
     }
 
+    /**
+     * Queue the given function to be sent via SOAP and wait for a response from the remote web service.
+     *
+     * ```php
+     * // advanced usage, see Proxy for recommended alternative
+     * $promise = $client->soapCall('ping', array('hello', 42));
+     * ```
+     *
+     * Note: This is considered *advanced usage*, you may want to look into using the [`Proxy`](#proxy) instead.
+     *
+     * ```php
+     * $proxy = new Proxy($client);
+     * $promise = $proxy->ping('hello', 42);
+     * ```
+     *
+     * @param string  $name
+     * @param mixed[] $args
+     * @return PromiseInterface Returns a Promise<mixed, Exception>
+     */
     public function soapCall($name, $args)
     {
         try {
@@ -55,28 +84,35 @@ final class Client
         );
     }
 
-    /**
-     * @param ResponseInterface $response
-     * @return mixed
-     * @internal
-     */
-    public function handleResponse(ResponseInterface $response)
-    {
-        return $this->decoder->decode((string)$response->getBody());
-    }
 
+    /**
+     * Returns an array of functions defined in the WSDL.
+     *
+     * It returns the equivalent of PHP's
+     * [`SoapClient::__getFunctions()`](http://php.net/manual/en/soapclient.getfunctions.php).
+     *
+     * @return string[]
+     */
     public function getFunctions()
     {
         return $this->encoder->__getFunctions();
     }
 
+    /**
+     * Returns an array of types defined in the WSDL.
+     *
+     * It returns the equivalent of PHP's
+     * [`SoapClient::__getTypes()`](http://php.net/manual/en/soapclient.gettypes.php).
+     *
+     * @return string[]
+     */
     public function getTypes()
     {
         return $this->encoder->__getTypes();
     }
 
     /**
-     * get location (URI) for given function name or number
+     * Returns the location (URI) of the given webservice `$function`.
      *
      * Note that this is not to be confused with the WSDL file location.
      * A WSDL file can contain any number of function definitions.
@@ -84,13 +120,23 @@ final class Client
      * However, technically each function can potentially use a different location.
      *
      * The `$function` parameter should be a string with the the SOAP function name.
-     * See also `getFunctions()` for a list of all available functions.
+     * See also [`getFunctions()`](#getfunctions) for a list of all available functions.
+     *
+     * ```php
+     * assert('http://example.com/soap/service' === $client->getLocation('echo'));
+     * ```
      *
      * For easier access, this function also accepts a numeric function index.
-     * It then uses `getFunctions()` internally to get the function
+     * It then uses [`getFunctions()`](#getfunctions) internally to get the function
      * name for the given index.
      * This is particularly useful for the very common case where all functions use the
      * same location and accessing the first location is sufficient.
+     *
+     * ```php
+     * assert('http://example.com/soap/service' === $client->getLocation(0));
+     * ```
+     *
+     * Passing a `$function` not defined in the WSDL file will throw a `SoapFault`.
      *
      * @param string|int $function
      * @return string
@@ -108,5 +154,15 @@ final class Client
 
         // encode request for given $function
         return (string)$this->encoder->encode($function, array())->getUri();
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return mixed
+     * @internal
+     */
+    public function handleResponse(ResponseInterface $response)
+    {
+        return $this->decoder->decode((string)$response->getBody());
     }
 }
--- src/Proxy.php
+++ src/Proxy.php
@@ -2,6 +2,27 @@
 
 namespace Clue\React\Soap;
 
+use React\Promise\PromiseInterface;
+
+/**
+ * The `Proxy` class wraps an existing [`Client`](#client) instance in order to ease calling
+ * SOAP functions.
+ *
+ * ```php
+ * $proxy = new Proxy($client);
+ * ```
+ *
+ * Each and every method call to the `Proxy` class will be sent via SOAP.
+ *
+ * ```php
+ * $proxy->myMethod($myArg1, $myArg2)->then(function ($response) {
+ *     // result received
+ * });
+ * ```
+ *
+ * Please refer to your WSDL or its accompanying documentation for details
+ * on which functions and arguments are supported.
+ */
 final class Proxy
 {
     private $client;
@@ -11,6 +32,11 @@ final class Proxy
         $this->client = $client;
     }
 
+    /**
+     * @param string  $name
+     * @param mixed[] $args
+     * @return PromiseInterface
+     */
     public function __call($name, $args)
     {
         return $this->client->soapCall($name, $args);
