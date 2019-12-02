c0a37b0155c3630313e3b93f1174bfc53c93d667
Fernando Robledo <fernando.robledo@masmovil.com>
2019-12-02 11:52:02 +0100

    Fix on ClientException message set

--- src/ClientException.php
+++ src/ClientException.php
@@ -10,14 +10,28 @@ class ClientException extends \SoapFault
     protected $response;
     protected $method;
 
+    /**
+     * ClientException constructor.
+     *
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     * @param string|null $method
+     * @param string|null $request
+     * @param string|null $response
+     */
     public function __construct($message = "", $code = 0, Throwable $previous = null, $method = null, $request = null, $response = null)
     {
         parent::__construct($message, $code, $previous);
 
+        $this->message = $message;
+
         if ($previous instanceof \SoapFault) {
-            $this->faultstring = isset($previous->faultstring) ? $previous->faultstring : null;
             $this->faultcode = isset($previous->faultcode) ? $previous->faultcode : null;
+            $this->faultstring = isset($previous->faultstring) ? $previous->faultstring : null;
+            $this->faultactor = isset($previous->faultactor) ? $previous->faultactor : null;
             $this->detail = isset($previous->detail) ? $previous->detail : null;
+            $this->faultname = isset($previous->faultactor) ? $previous->faultname : null;
         }
 
         $this->method = $method;
