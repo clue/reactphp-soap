62b8e6b6c07c37cf5f69b05e5db76c132e28642d
Simon Frings <simon@clue.engineering>
2020-05-19 18:11:35 +0200

    Fix Cancellation on test with expected Exception

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -9,6 +9,10 @@ use Psr\Http\Message\RequestInterface;
 
 class ClientTest extends TestCase
 {
+
+    /**
+     * @expectedException SoapFault
+     */
     public function testConstructorThrowsWhenUrlIsInvalid()
     {
         if (extension_loaded('xdebug')) {
